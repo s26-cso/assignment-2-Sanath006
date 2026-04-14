1ed6e14a89dac1adca9ab23704b769832ea247a9
Sanath006 <sanathhegde2018@gmail.com>
2026-04-14 23:26:54 +0530

    Update q4.c

---
+++ q4/q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+int main(){
+    char op[6];
+    int a , b;
+
+    while(scanf("%5s %d %d" , op , &a , &b) == 3){
+        char libname[20];
+        snprintf(libname , sizeof(libname) , "./lib%s.so" , op);
+        void *handle = dlopen(libname , RTLD_LAZY);
+        int(*func)(int , int);
+        *(void **)(&func) = dlsym(handle, op);
+        int result = func(a, b);
+        printf("%d\n", result);
+        dlclose(handle);
+    }
+}
